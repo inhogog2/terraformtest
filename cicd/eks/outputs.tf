1f0706be7537c29c31a7262be3136cfe904b7f4c
inho <inhogog2@netlox.io>
2023-08-04 05:46:49 +0000

    output added

--- cicd/eks/outputs.tf
+++ cicd/eks/outputs.tf
@@ -6,6 +6,20 @@ output "region" {
   value       = var.region
 }
 
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
 
 output "loxilb_private_ip" {
   description = "loxilb private ip address"
@@ -26,3 +40,5 @@ output "host_public_ip" {
   description = "host public ip address"
   value       = module.ec2_instance2.public_ip
 }
+
+
