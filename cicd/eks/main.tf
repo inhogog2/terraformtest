7c576171df1629ca947e6111f75a812ecf102ade
inhogog2 <75723069+inhogog2@users.noreply.github.com>
2025-08-21 13:40:30 +0900

    Update main.tf

--- cicd/eks/main.tf
+++ cicd/eks/main.tf
@@ -40,23 +40,38 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.1.0"
+  version = "~> 21.0"
 
   name    = local.cluster_name
   kubernetes_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
   endpoint_public_access = true
-
+  enable_cluster_creator_admin_permissions = true
   eks_managed_node_groups = {
-    node = {
-      name = "node-group"
-      ami_type = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"]
-      min_size     = 1
+   example = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+
+      min_size     = 2
       max_size     = 3
       desired_size = 2
+
+      vpc_security_group_ids = [
+        module.security-group.security_group_id,
+        module.eks.node_security_group_id
+      ]
     }
   }
 }
